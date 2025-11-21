73e5fdcd2eb28bc1a291c84b49fb7ceaee1ed05a
Ernests Lavrinovics <ernestslavrinovics@gmail.com>
2025-11-21 15:19:42 +0100

    update

--- label_studio.py
+++ label_studio.py
@@ -83,7 +83,7 @@ def stop_label_studio(process):
 if __name__ == "__main__":
     # Start the server with default settings
     server_process = start_label_studio(
-        host="130.225.198.209",
+        host="127.0.0.1",
         port=8081,
         data_dir="tmp/"
     )
